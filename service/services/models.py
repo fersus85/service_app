55833795944a997b6714fec52a1baf433e25077f
Deriabin E <Deriabin1985@gmail.com>
2023-03-06 15:48:43 +0300

    add apps, create db postgres, create models, api endpoint

---
+++ service/services/models.py
@@ -0,0 +1,32 @@
+from django.core.validators import MaxValueValidator
+from django.db import models
+from clients.models import Client
+
+
+# Create your models here.
+class Service(models.Model):
+    subscribe_name = models.CharField(max_length=50)
+    full_price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'Plan: {self.subscribe_name}'
+
+
+class Plan(models.Model):
+    PLAN_TYPES = (
+        ('full', 'Full'),
+        ('student', 'Student'),
+        ('discount', 'Discount'),
+    )
+
+    plan_type = models.CharField(choices=PLAN_TYPES, max_length=10)
+    discount_percent = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
+
+    def __str__(self):
+        return f'Plan: {self.plan_type}'
+
+
+class Subscription(models.Model):
+    client = models.ForeignKey(Client, related_name='subscription', on_delete=models.PROTECT)
+    service = models.ForeignKey(Service, related_name='subscription', on_delete=models.PROTECT)
+    plan = models.ForeignKey(Plan, related_name='subscription', on_delete=models.PROTECT)
